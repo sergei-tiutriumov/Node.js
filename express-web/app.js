f1155d06c9db5be7ccc458091b17fdc65c91dda1
Serj Che <sergei.tiutriumov@gorfax.ru>
2024-09-01 21:47:44 +0300

    picnicss

--- express-web/app.js
+++ express-web/app.js
@@ -1,10 +1,13 @@
 import express from  'express';
 import { config } from 'dotenv';
 import './source/models/_loaddatabase.js';
-import router from './sourse/router.js';
+import router from './source/routes.js';
 
 config();
 const port = process.env.PORT || 8000;
 const app = express();
+app.locals.appTitle = process.env.APPTITLE || "Express";
+app.set('view engine', 'ejs');
+app.set('views', './source/templates');
 app.listen(port);
 app.use('/', router);
--- express-web/source/controllers/todos.js
+++ express-web/source/controllers/todos.js
@@ -1,66 +1,28 @@
-<<<<<<< HEAD
 import { getList, getItem } from '../models/todos.js';
-=======
-import { getList } from '../models/todos.js';
->>>>>>> cf9af8ae5374dcf7239aad4cbda66c42e56cb990
 
 export function mainPage(req, res) {
-    const list = getList();
-    let s = '<!doctype html>' +
-            '<html>' +
-            '   <head>' +
-            '       <meta charset="UTF-8">' +
-            '       <title>Список запланированных дел</title>' +
-            '   </head>' +
-            '   <body>' +
-            '       <h1>Запланированные дела</h1>';
-    for (let t of list) {
-        const date = new Date(t.createdAt);
-        s += `  <h2><a href="/${t._id}/">${t.title}</a><h2>` +
-             `  <p>${t.desc}</p>` +
-             `  <p>${date.toLocaleString() }</p> ` +
-             '  <p>&nbsp;</p>';
-    }
-    
-    s +=    '   </body>' +
-            '</html>';
-    res.send(s);
+    res.render('main', {
+        todos: getList(),
+        title: 'Главная'
+        });
 }
 
-export function  detailedPage(req,res) {
+export function  detailPage(req,res) {
         const t = getItem(req.params.id);
 
         if(!t) {
                 errorPage(req,res);
                 return;
         }
-
-        const data = new Date(t.createdAt);
-        res.send('<!doctype html>' +
-                 '<html>' +
-                 '  <head>' +
-                 '      <meta charset="UTF-8">' +
-                 `       <title>${t.title} :: Список запланированных дел </title>` +
-                 '  </head>' +
-                 '  <body>' +
-                 `       <h1>${t.title}</h1>` +
-                 `       <p>${t.desc}</p>` +
-                 `       <p>Создано: ${date.toLocaleString() }</p>` +
-                 '  </body>' +
-                 '</html>');
+        res.render('detail', {
+                todo: t,
+                title: t.title
+        });
 }
 
 function errorPage(req, res) {
         res.status(404);
-        res.send('<!doctype html>'+
-                 '<html>' +
-                 '  <head>' +
-                 '      <meta charset="UTF-8">' +
-                 '      <title>Ошибка</title>' +
-                 '  <head>' +
-                 '  <body>' + 
-                 '    <h1>Ошибка!</h1>' +
-                 '    <p>Запрошенная страница не существует.</p>' +
-                 '  </body>' +
-                 '</html>');
+        res.render('404', {
+                title: 'Ошибка!'
+        });
 }
