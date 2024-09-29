2bb179bc956f4ceec16c298f3171699529cdca8d
Serj Che <sergei.tiutriumov@gorfax.ru>
2024-09-29 21:53:40 +0300

    добавлено сообщение об ошибке с заголовком

--- express-web/source/middleware.js
+++ express-web/source/middleware.js
@@ -1,15 +1,32 @@
 import { matchedData, validationResult } from 'express-validator';
 
-export function handleErrors(req, res, next) {
+export async function handleErrors(req, res, next) {
     const r = validationResult(req);
-    if (!r.isEmpty())
+    if (!r.isEmpty()) {
+        await req.flash('errors', r.mapped());
+        await req.flash('body', req.body);
         res.redirect('back');
-    else {
+    } else {
         req.body = matchedData(req);
         next();
     }
 }
+
+export async function getErrors(req, res, next) {
+    res.locals.errors = await req.getFlash('errors') || {};
+    res.locals.body = await req.getFlash('body') || {};
+    next();
+}
+
 export function requestToContext (req, res, next) {
     res.locals.req = req;
+    next();
+}
+
+export function extendFlashAPI(req, res, next) {
+    req.getFlash = async function(name) {
+        const d = await this.consumeFlash(name);
+        return d.length > 0 ? d[0] : undefined;
+    }
     next();
 }
--- express-web/source/routes.js
+++ express-web/source/routes.js
@@ -1,19 +1,42 @@
 import { Router, urlencoded, static as staticMiddleware } from 'express';
 import methodOverride from 'method-override'; 
 import { mainPage, detailPage, addPage, add, setDone, remove, setOrder } from './controllers/todos.js';
-import { requestToContext, handleErrors } from './middleware.js';
+import { requestToContext, handleErrors, extendFlashAPI, getErrors } from './middleware.js';
 import { todoV } from './validators.js';
 import { mainErrorHandler, error500Handler } from './error-handlers.js';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import _FileStore from 'session-file-store';
+import { flash } from 'express-flash-message';
 
+const FileStore = _FileStore(session);
 const router = Router();
 router.use(staticMiddleware('public'));
 router.use(urlencoded({ extended:true}));
 router.use(methodOverride('_method'));
 router.use(requestToContext);
 router.use(cookieParser());
+router.use(session({
+    store: new FileStore({
+        path: './storage/sessions',
+        reapAsync: true,
+        reapSyncFallback: true,
+        fallbackSessionFn: () => {
+            return {};
+        },
+        logFn: () => {}
+    }),
+    secret: 'abcdefgh',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
+router.use(flash({sessionKeyName: 'flash-message'}));
+router.use(extendFlashAPI);
 
-router.get('/add', addPage);
+router.get('/add', getErrors, addPage);
 router.post('/add', todoV, handleErrors, add);
 router.get('/:id', detailPage);
 router.put('/:id', setDone);
